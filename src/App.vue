8f3eb5a97bd3692832804eb63dcc07e65826073a
Robot <robotbpla@gmail.com>
2025-04-26 18:41:42 +0300

    fixed styles and add container block for TaskBlock

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="app-layout" :class="{ 'no-sidebar': $route.meta.hideSidebar }">
+  <div class="app-layout"
+  :class="{ 'no-sidebar': $route.meta.hideSidebar }"
+  :style="{
+    '--bg': background_color,
+    '--border': border_color,
+    '--text': text_color,
+    '--kruglik': kruglik_size,
+    '--grad_clr_l': grad_color_left,
+    '--grad_clr_r': grad_color_right,
+  }">
     <!-- 🧱 Sidebar всегда сбоку -->
     <Sidebar v-if="!$route.meta.hideSidebar" />
 
     <!-- Вся остальная часть страницы -->
-    <div class="main-and-console">
+    <div class="main-and-console" :class="{ 'with-header': $route.meta.hasHeader }">
       <!-- Скроллим здесь -->
       <div class="main-scroll-zone">
         <router-view :key="$route.fullPath" v-slot="{ Component }">
@@ -24,6 +33,23 @@
 <script setup>
 import Sidebar from '@/components/Sidebar.vue'
 import { consoleOutput, consoleVisible } from '@/store/console'
+import { provide } from 'vue'
+
+
+const background_color = "#ffffff"
+const border_color = "#8800cc"
+const text_color = "#000000"
+const kruglik_size = "16px"
+const grad_color_left = "#581170"
+const grad_color_right = "#1D012A"
+
+provide('background_color', background_color)
+provide('border_color', border_color)
+provide('text_color', text_color)
+provide('kruglik_size', kruglik_size)
+provide('grad_color_left', grad_color_left)
+provide('grad_color_right', grad_color_right)
+
 </script>
 
 <style>
@@ -49,27 +75,28 @@ html, body {
   flex-grow: 1;
   height: 100%;
   overflow: hidden;
-  padding-top: 0; /* по умолчанию НЕТ отступа */
 }
 
-.main-and-console.with-header {
+.main-and-console.with-header .main-scroll-zone{
   padding-top: 60px; /* добавляем отступ, если есть шляпа */
 }
 
 .main-scroll-zone {
   flex-grow: 1;
   overflow: hidden;
+  min-height: 0;
 }
 
 .bottom-console {
+  flex: 0 0 200px;
   background: #111;
   color: #0f0;
   font-family: monospace;
   padding: 12px 20px;
-  height: 200px;
   overflow-y: auto;
   border-top: 2px solid #444;
   white-space: pre-wrap;
+  box-sizing: border-box;
 }
 
 .fade-enter-active,
