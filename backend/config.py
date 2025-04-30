a8ae00b55e121ef4464e4782c92b7c854fa9ce42
George <2georgedenisov@gmail.com>
2025-04-30 19:20:09 +0300

    Update DB to SQLite

--- backend/config.py
+++ backend/config.py
@@ -4,5 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    USE_SQLITE = os.getenv("USE_SQLITE", "1") == "1"
+
+    if USE_SQLITE:
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'app.db')}"
+    else:
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
